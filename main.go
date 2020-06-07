74cd5d42870ffe8415051693b88626fb74c4f95c
Valentin Witzeneder <valentin.witzeneder@dynatrace.com>
2020-06-07 14:53:16 +0200

    remove environment variables

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import "os"
 func main() {
 	a := App{}
 	a.Initialize(
-		os.Getenv("APP_DB_USERNAME"),
-		os.Getenv("APP_DB_PASSWORD"),
-		os.Getenv("APP_DB_NAME"))
+		"postgres",
+		"",
+		"postgres")
 
 	a.Run(":8010")
 }
--- main_test.go
+++ main_test.go
@@ -18,9 +18,9 @@ var a App
 
 func TestMain(m *testing.M) {
     a.Initialize(
-        os.Getenv("APP_DB_USERNAME"),
-        os.Getenv("APP_DB_PASSWORD"),
-        os.Getenv("APP_DB_NAME"))
+        "postgres",
+        "",
+        "postgres")
 
     ensureTableExists()
     code := m.Run()
